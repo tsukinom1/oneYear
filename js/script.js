533aef81f90cdb42b1fb6542cda8bc8ffd50b1cf
giventsuki <alkuat.zh@gmail.com>
2024-05-28 17:03:22 +0500

    2

--- js/script.js
+++ js/script.js
@@ -2,35 +2,35 @@ AOS.init({
     disable: false,
 });
 
-// document.body.classList.add('body-scroll-lock');
+document.body.classList.add('body-scroll-lock');
 
-// const registerForm = document.getElementById('registerForm');
-// const nameInput = document.getElementById('name');
-// const passwordInput = document.getElementById('password');
-// const closeButton = document.getElementById('close');
-// const formButton = document.getElementById('formButton');
-// const content = document.getElementById('content');
-//
-//
-//
-//
-// closeButton.onclick = function () {
-//     registerForm.style.display = 'none'
-//     document.body.classList.remove('body-scroll-lock');
-// }
-//
-//
-// formButton.onclick = function (e) {
-//     e.preventDefault();
-//     if(nameInput.value === 'miss D' && passwordInput.value === 'jarqynym') {
-//         alert('Welcome, my kotik <3');
-//         registerForm.style.display = 'none'
-//         document.body.classList.remove('body-scroll-lock');
-//         content.classList.remove('none')
-//     } else {
-//         alert('OPS, incorrect answer');
-//     }
-// };
+const registerForm = document.getElementById('registerForm');
+const nameInput = document.getElementById('name');
+const passwordInput = document.getElementById('password');
+const closeButton = document.getElementById('close');
+const formButton = document.getElementById('formButton');
+const content = document.getElementById('content');
+
+
+
+
+closeButton.onclick = function () {
+    registerForm.style.display = 'none'
+    document.body.classList.remove('body-scroll-lock');
+}
+
+
+formButton.onclick = function (e) {
+    e.preventDefault();
+    if(nameInput.value === 'miss D' && passwordInput.value === 'jarqynym') {
+        alert('Welcome, my kotik <3');
+        registerForm.style.display = 'none'
+        document.body.classList.remove('body-scroll-lock');
+        content.classList.remove('none')
+    } else {
+        alert('OPS, incorrect answer');
+    }
+};
 
 
 
